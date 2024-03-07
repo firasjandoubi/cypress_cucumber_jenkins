bba3db59f9b3f238a2de202a90a1d4fc5aa6f73f
firasjandoubi <firas.jandoubi@esprit.tn>
2024-03-07 09:44:35 +0100

    cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,36 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const cypressOnFix = require('cypress-on-fix');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
-  supportFile: false,
-  reporter: 'mochawesome',
+  
+  e2e: {
+    specPattern:'cypress/e2e/**/*.feature',
+    projectId: "wkv5u3",
+    async setupNodeEvents(on, config) {
+      on = cypressOnFix(on);
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      
+      // implement node event listeners here
+      on(
+        'file:preprocessor',
+        cucumber()
+      );
+      
+    },
+    
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  retries: 1,
   reporterOptions:{
     charts: true,
     embeddedScreenshots:true
   },
-  e2e: {
-    specPattern:'cypress/e2e/**/*.feature',
-    projectId: "wkv5u3",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on('file:preprocessor', cucumber());
-    },
   },
   
 });
